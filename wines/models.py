7b107fbb45c17c66a1971dd0447707f071da4c5d
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2025-07-21 14:04:18 +0300

    add models for wine

--- wines/models.py
+++ wines/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Wine(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.FloatField(null=True, blank=True)
+    wine_type = models.CharField(max_length=100, blank=True)
+    abv = models.FloatField(null=True, blank=True)
+    vintage = models.CharField(max_length=50, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    region = models.CharField(max_length=100, blank=True)
+    grape = models.CharField(max_length=100, blank=True)
+    characteristics = models.TextField(blank=True)
+    style = models.CharField(max_length=100, blank=True)
+    capacity = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
